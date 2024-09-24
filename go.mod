8a4137bfe1a6903cc00339afdfbad7bb6213617f
Carlana Johnson <mail@carlana.net>
2024-09-24 13:49:56 -0400

    Use Filippo's code

--- go.mod
+++ go.mod
@@ -4,6 +4,12 @@ go 1.23
 
 require (
 	github.com/carlmjohnson/exitcode v0.20.2
-	github.com/carlmjohnson/flagx v0.22.2
-	github.com/carlmjohnson/versioninfo v0.22.5
+	github.com/microcosm-cc/bluemonday v1.0.27
+	github.com/progrium/darwinkit v0.5.0
+)
+
+require (
+	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/gorilla/css v1.0.1 // indirect
+	golang.org/x/net v0.26.0 // indirect
 )
--- pasteboard/pasteboard.go
+++ pasteboard/pasteboard.go
@@ -1,80 +1,27 @@
 package pasteboard
 
 import (
-	"flag"
-	"fmt"
-	"io"
-	"log"
-	"os"
-
-	"github.com/carlmjohnson/flagx"
-	"github.com/carlmjohnson/flagx/lazyio"
-	"github.com/carlmjohnson/versioninfo"
+	"github.com/microcosm-cc/bluemonday"
+	"github.com/progrium/darwinkit/macos/appkit"
 )
 
 const AppName = "Rich Paste"
 
 func CLI(args []string) error {
-	var app appEnv
-	err := app.ParseArgs(args)
-	if err != nil {
-		return err
-	}
-	if err = app.Exec(); err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-	}
-	return err
-}
+	pboard := appkit.Pasteboard_GeneralPasteboard()
+	html := pboard.StringForType(appkit.PasteboardTypeHTML)
+	p := bluemonday.StrictPolicy()
 
-func (app *appEnv) ParseArgs(args []string) error {
-	fl := flag.NewFlagSet(AppName, flag.ContinueOnError)
-	src := lazyio.FileOrURL(lazyio.StdIO, nil)
-	app.src = src
-	fl.Var(src, "src", "source file or URL")
-	app.Logger = log.New(io.Discard, AppName+" ", log.LstdFlags)
-	flagx.BoolFunc(fl, "verbose", "log debug output", func() error {
-		app.Logger.SetOutput(os.Stderr)
-		return nil
-	})
-	fl.Usage = func() {
-		fmt.Fprintf(fl.Output(), `richpaste - %s
+	p.AllowElements("br", "ol", "ul", "li", "a", "b", "i", "strong",
+		"em", "u", "s", "sub", "sup", "code", "pre", "blockquote",
+		"h1", "h2", "h3", "h4", "h5", "h6")
+	p.AllowAttrs("href").OnElements("a")
+	p.AllowAttrs("style").OnElements("span")
+	p.AllowStyles("font-weight", "font-style", "text-decoration").Globally()
 
-Paste rich text in Mac OS
+	html = p.Sanitize(html)
 
-Usage:
-
-	richpaste [options]
-
-Options:
-`, versioninfo.Version)
-		fl.PrintDefaults()
-	}
-	if err := fl.Parse(args); err != nil {
-		return err
-	}
-	if err := flagx.ParseEnv(fl, AppName); err != nil {
-		return err
-	}
+	pboard.ClearContents()
+	pboard.SetStringForType(html, appkit.PasteboardTypeHTML)
 	return nil
-}
-
-type appEnv struct {
-	src io.ReadCloser
-	*log.Logger
-}
-
-func (app *appEnv) Exec() (err error) {
-	app.Println("starting")
-	defer func() { app.Println("done") }()
-
-	n, err := io.Copy(os.Stdout, app.src)
-	defer func() {
-		e2 := app.src.Close()
-		if err == nil {
-			err = e2
-		}
-	}()
-	app.Printf("copied %d bytes\n", n)
-
-	return err
 }
